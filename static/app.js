bdc46f19e143d563afc5adcf9de1968cdd000116
Chino Arror <103983438+ChinoArror@users.noreply.github.com>
2025-12-24 01:29:21 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -36,6 +36,21 @@ async function login() {
   }
 }
 
+async function renderHome() {
+  const res = await fetch('/api/subjects');
+  const data = await res.json();
+
+  app.innerHTML = `
+    <div class="subject-grid">
+      ${data.data.map(s => `
+        <div class="card" onclick="openSubject('${s.code}')">
+          ${s.title}
+        </div>
+      `).join('')}
+    </div>
+  `;
+}
+
 async function openSubject(code) {
   const res = await fetch('/api/roster');
   const data = await res.json();
